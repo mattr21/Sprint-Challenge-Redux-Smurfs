c6398de276c0588b36d7f7db8bc82cfd6a369cc5
mattr21 <matt.rothstein21@gmail.com>
2019-03-08 08:22:51 -0800

    imported action types and setup casses for fetching and adding in reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,12 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { 
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAIL,
+
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAIL,
+} from '../actions'
 
  const initialState = {
    smurfs: [],
@@ -13,7 +19,43 @@
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_SMURFS_START:
+    return {
+      ...state,
+      fetchingSmurfs: true,
+      error: null
+    };
+  case FETCH_SMURFS_SUCCESS:
+    return {
+      ...state,
+      smurfs: [...state.smurfs, ...action.payload],
+      fetchingSmurfs: false
+    };
+  case FETCH_SMURFS_FAIL:
+    return {
+      ...state,
+      fetchingSmurfs: false,
+      error: action.payload
+    };
 
+  case ADD_SMURF_START:
+    return {
+      ...state,
+      addingSmurf: true,
+      error: null
+    };
+  case ADD_SMURF_SUCCESS:
+    return {
+      ...state,
+      notes: [...state.smurfs, ...action.payload],
+      addingSmurf: false
+    };
+  case ADD_SMURF_FAIL:
+    return {
+      ...state,
+      addingSmurf: false,
+      error: action.payload
+    };
     default:
       return state;
   }
