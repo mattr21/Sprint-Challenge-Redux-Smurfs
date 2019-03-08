cb0ef2df37b2f5e9be6e546225e44649705720e2
mattr21 <matt.rothstein21@gmail.com>
2019-03-08 09:06:34 -0800

    displayed smurfs, added a form to add new smurfs, and hooked everything up in components App,js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,94 @@
 import React, { Component } from 'react';
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import { connect } from 'react-redux';
+import { getSmurfs, addSmurf } from '../actions';
+
+
 class App extends Component {
+  state = {
+    smurf: {
+      name: '',
+      age: '',
+      height: '',
+    }
+  }
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
+  changeHandler = e => {
+    e.persist();
+    let value = e.target.value;
+    if (e.target.name === "age") {
+      value = parseInt(value, 10);
+    }
+    this.setState(prevState => ({
+      smurf: {
+        ...prevState.smurf,
+        [e.target.name]: value
+      }
+    }));
+  };
+
+  submitHandler = e => {
+    // e.preventDefault();
+    this.props.addSmurf(this.state.smurf);
+    this.setState({ 
+      smurf: {
+        name: '',
+        age: '',
+        height: ''
+      }});
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <form onSubmit={this.submitHandler}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Smurf Name"
+            value={this.state.smurf.name}
+            onChange={this.changeHandler}
+            required
+          />
+          <input
+            type="number"
+            name="age"
+            placeholder="Smurf Age"
+            value={this.state.smurf.age}
+            onChange={this.changeHandler}
+            required
+          />
+          <input
+            type="text"
+            name="height"
+            placeholder="Smurf Height"
+            value={this.state.smurf.height}
+            onChange={this.changeHandler}
+            required
+          />
+          <button>Add Smurf</button>
+        </form>
+        {this.props.smurfs.map(smurf => (
+          <div>
+              <p>{smurf.name}</p>
+              <p>{smurf.age}</p>
+              <p>{smurf.height}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs
+})
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs, addSmurf }
+)(App);
